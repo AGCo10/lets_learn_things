d80f878127de31097a0d8b0f31bffdd2e10899d4
Adam Gaines <adam.j.gaines.mil@swf.army.mil>
2025-08-01 14:35:45 -0500

    added two lines

--- hello_kevin.sh
+++ hello_kevin.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 
 echo "Hello Kevin"
+echo "KEVIN!"
+echo "--Proceeds to fly across the world to find their child home alone after violently and psychologically torturing home intruders."
